e097692ec5d63c498ac235a8d0d1cfcdc2de5019
Willownezz <luizdgarcia@gmail.com>
2019-08-30 11:26:38 -0700

    makefile ver5

---
+++ _makeProj/src/hellofunc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <hellomake.h>
+
+void myPrintHelloMake(void)
+{
+    printf("Hello makefiles!\n");
+    printf("It WORKED!\n");
+    printf("It WORKED!!!!\n");
+
+    return;
+}
