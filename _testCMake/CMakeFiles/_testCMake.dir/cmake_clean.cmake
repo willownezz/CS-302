ff48738367c9c9b2341aea6d5accc78bedb5b4f7
Willownezz <luizdgarcia@gmail.com>
2019-09-09 09:03:51 -0700

    cmake practice

---
+++ _testCMake/CMakeFiles/_testCMake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_testCMake.dir/main.cpp.o"
+  "_testCMake.pdb"
+  "_testCMake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_testCMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ _testCMake/CMakeFiles/helloWorldCMake.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloWorldCMake.dir/main.cpp.o"
+  "helloWorldCMake.pdb"
+  "helloWorldCMake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloWorldCMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
